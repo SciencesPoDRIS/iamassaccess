6cb037d2492e14bffa927ede1e7d4ca3304f745e
Diégo Antolinos-Basso <diego.antolinosbasso@sciencespo.fr>
2016-01-20 17:07:46 +0100

    Add comments in code

--- iaupload.py
+++ iaupload.py
@@ -15,11 +15,13 @@ if os.path.exists(conf_file) :
 else :
 	print 'No conf file'
 
+# Walk the dir containing the files to upload
 files = []
 for root, dirs, docs in os.walk(sys.argv[1]):
 	for doc in docs:
 		files.append(os.path.join(root, doc))
 
+# Headers : needs to be investigated and handled
 headers = dict()
 headers['x-archive-auto-make-bucket'] = 1
 headers['x-archive-size-hint'] = 1397759
@@ -37,5 +39,5 @@ item = internetarchive.get_item('EL065_L_1973_03_006_04_2_PF')
 # Upload a single file to an item.
 item.upload_file(files[0], headers=headers, metadata=metadata, access_key=conf["access_key"], secret_key=conf["secret_key"])
 
-# Modify metadata : replace an existing one or create new metadata
+# Modify metadata : modify an existing one or create new metadata
 item.modify_metadata(metadata, access_key=conf["access_key"], secret_key=conf["secret_key"])
