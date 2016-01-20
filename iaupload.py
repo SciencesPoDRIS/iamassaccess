2b2441698881087e7ad6bc791f855b4ba5cff4c4
Diégo Antolinos-Basso <diego.antolinosbasso@sciencespo.fr>
2016-01-20 15:30:51 +0100

    Close #1 : authentication on upload_file with credentials

--- iaupload.py
+++ iaupload.py
@@ -23,22 +23,20 @@ for root, dirs, docs in os.walk(sys.argv[1]):
 headers = dict()
 headers['x-archive-auto-make-bucket'] = 1
 headers['x-archive-size-hint'] = 1397759
-
-
 print headers
 
-# metadata = dict()
-# metadata['mediatype'] = 'pdf'
+metadata = dict()
+# metadata['mediatype'] = 'image'
 # metadata['creator'] = 'Bibliothèque de SciencesPo'
 # metadata['Page sTitle'] = 'Législatives 1973'
-# metadata['Language'] = 'French'
+metadata['Language'] = 'French'
 # metadata['Year'] = '1973'
-# metadata['Collections'] = ['archiveselectoralesducevipof', 'additional_collections']
-# metadata['Usage'] = 'Attribution-Noncommercial-No Derivative Works 3.0'
+# metadata['Collections'] = ['test_collections']
+metadata['Usage'] = 'Attribution-Noncommercial-No Derivative Works 3.0'
 # metadata['Topics'] = ['France', 'Assemblée nationale', 'Elections législatives', 'Ve République']
 
-item = internetarchive.Item('Pictures')
+item = internetarchive.get_item('Newbucketohyeah')
 
 print item.exists
 
-item.upload_file(files[0], headers=headers)
+item.upload_file(files[0], headers=headers, metadata=metadata, access_key=conf["access_key"], secret_key=conf["secret_key"])
