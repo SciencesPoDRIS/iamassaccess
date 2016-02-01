4e5499c539f81aad9d3e916beba2d0f843e67591
Anne Lhote <anne.lhote@gmail.com>
2016-02-01 12:02:09 +0100

    Change metadata format from json to csv

--- iamassaccess.py
+++ iamassaccess.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import csv
 import internetarchive, json, logging, os, sys
 
 # Load conf file
@@ -9,56 +10,64 @@ log_folder = 'log'
 log_level = logging.DEBUG
 item_id = 'new_al_item'
 
-# Check that log folder exists, else create it
-if not os.path.exists(log_folder) :
-	os.makedirs(log_folder)
-# Create log file path
-log_file = os.path.join(log_folder, sys.argv[0].replace('.py', '.log'))
-# Init logs
-logging.basicConfig(filename = log_file, filemode = 'a+', format = '%(asctime)s  |  %(levelname)s  |  %(message)s', datefmt = '%m/%d/%Y %I:%M:%S %p', level = log_level)
-logging.info('Start')
+#
+# Main
+#
+if __name__ == '__main__':
+	# Check that log folder exists, else create it
+	if not os.path.exists(log_folder) :
+		os.makedirs(log_folder)
+	# Create log file path
+	log_file = os.path.join(log_folder, sys.argv[0].replace('.py', '.log'))
+	# Init logs
+	logging.basicConfig(filename=log_file, filemode='a+', format='%(asctime)s  |  %(levelname)s  |  %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=log_level)
+	logging.info('Start')
 
-if os.path.exists(conf_file) :
-	with open(conf_file) as f :
-		conf = json.load(f)
-else :
-	logging.error('No conf file')
-	sys.exit(0)
+	if os.path.exists(conf_file) :
+		with open(conf_file) as f :
+			conf = json.load(f)
+	else :
+		logging.error('No conf file')
+		sys.exit(0)
 
-# Walk the dir containing the files to upload
-if len(sys.argv) < 2 :
-	logging.error('No folder specified')
-	sys.exit(0)
+	if len(sys.argv) < 3 :
+		logging.error('No folder specified')
+		sys.exit(0)
 
-files = []
-for dirpath, dirnames, filenames in os.walk(sys.argv[1]) :
-	for filename in filenames :
-		files.append(os.path.join(dirpath, filename))
-		logging.info('File append : ' + filename)
+	# Walk the dir containing the files to upload
+	files = []
+	for dirpath, dirnames, filenames in os.walk(sys.argv[1]) :
+		for filename in filenames :
+			files.append(os.path.join(dirpath, filename))
+			logging.info('File append : ' + filename)
 
-# Headers : add additional HTTP headers to the request if needed
-# RTFM : http://archive.org/help/abouts3.txt
-headers = dict()
+	# Headers : add additional HTTP headers to the request if needed
+	# RTFM : http://archive.org/help/abouts3.txt
+	headers = dict()
 
-# Load metadata as json file
-# ToDo : all the keys have to be in lowercase
-if os.path.exists(os.path.join(sys.argv[1], 'metadata.json')) :
-	with open(os.path.join(sys.argv[1], 'metadata.json')) as metadata_file :
-		metadata = json.load(metadata_file)
-# If no metadata file for this item
-else :
+	# Load metadata from CSV file
+	# ToDo : all the keys have to be in lowercase
 	metadata = dict()
-	logging.info('No metadata file for item : ' + item_id)
+	if os.path.exists('test/metadata.csv') :
+		with open('test/metadata.csv', 'rb') as csvfile :
+			spamreader = csv.reader(csvfile, delimiter=',', quotechar='"', skipinitialspace=True)
+			keys = spamreader.next()
+			for values in spamreader :
+				metadata[values[0]] = dict()
+				for x, y in enumerate(values[1:]) :
+					metadata[values[0]][keys[x + 1].lower()] = y
+	else :
+		logging.info('No metadata file for item : ' + item_id)
 
-# Get the item with unique identifier. The item will be created if it does not exist.
-item = internetarchive.get_item(item_id)
+	# Get the item with unique identifier. The item will be created if it does not exist.
+	item = internetarchive.get_item(item_id)
 
-# Upload multiple files to an item.
-item.upload(files, metadata=metadata, headers=headers, access_key=conf['access_key'], secret_key=conf['secret_key'])
-logging.info('Files uploaded for item : ' + item_id)
+	# Upload multiple files to an item.
+	# item.upload(files, metadata=metadata, headers=headers, access_key=conf['access_key'], secret_key=conf['secret_key'])
+	logging.info('Files uploaded for item : ' + item_id)
 
-# Modify metadata : modify an existing one or create new metadata
-item.modify_metadata(metadata, access_key=conf['access_key'], secret_key=conf['secret_key'])
-logging.info('Metadata modified for item : ' + item_id)
+	# Modify metadata : modify an existing one or create new metadata
+	# item.modify_metadata(metadata, access_key=conf['access_key'], secret_key=conf['secret_key'])
+	logging.info('Metadata modified for item : ' + item_id)
 
-logging.info('End')
+	logging.info('End')
