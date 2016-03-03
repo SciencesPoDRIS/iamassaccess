7e16af9db4c576773b6b95e2721f2ca2e245244b
Diégo Antolinos-Basso <diego.antolinosbasso@sciencespo.fr>
2016-03-03 16:23:26 +0100

    Removes possibility of uploading without metadata (there is always mandatory metadata) + corrects loading of metadata csv file

--- iamassaccess.py
+++ iamassaccess.py
@@ -60,14 +60,10 @@ def walk_files_upload(folder):
 	items = os.listdir(folder)
 	
 	if "metadata.csv" in items:
-		metadata = load_csv_metadata_file('metadata.csv')
+		metadata = load_csv_metadata_file(os.path.join(folder, 'metadata.csv'))
 	else:
-		no_metadata = raw_input('Mode chosen is CREATE and no metadata provided.\nDo you want to upload your files without metadata?\n[Y/n]').lower()
-		if no_metadata != 'y' and no_metadata != 'yes' and no_metadata != 'Yes':
-			logging.error('User chose to exit because no metadata file provided for the specified files')
-			sys.exit(0)
-		else:
-			logging.error('User chose to upload files without metadata')
+		logging.error('No metadata file provided')
+		sys.exit(0)
 	
 	files_dic = {}
 	for item in items:
@@ -81,11 +77,13 @@ def walk_files_upload(folder):
 						folder_structure_problem()
 					else:
 						files_dic[item] = files
+	
 	# TODO : check if there's metadata for each file in the folder, vice-versa
 	return metadata, files_dic
 
 # Load a csv file (used for the metadata)
 def load_csv_metadata_file(metadata):
+	print metadata
 	if os.path.exists(os.path.join(metadata)):
 		metadata_dict = defaultdict(lambda : defaultdict())
 		with open(metadata) as metadata_file :
@@ -118,6 +116,10 @@ def createItems(folder, metadata_file=None):
 	files = walk_files_folder(folder)
 	if metadata_file != None:
 		metadata = load_metadata_file(metadata_file)
+
+# TODO
+def updateItems(metadata_file):
+	metadata = load_csv_metadata_file(metadata_file)
 
 # Load conf file
 conf_file = os.path.join('conf', 'conf.json')
@@ -153,28 +155,15 @@ args = parser.parse_args()
 if args.mode == 'create' and args.files is None:
 	logging.error('Mode chosen is CREATE and no files are provided to upload')
 	sys.exit(0)
-elif args.mode == 'create' and args.metadata is None:
-	no_metadata = raw_input('Mode chosen is CREATE and no metadata provided.\nDo you want to upload your files without metadata?\n[Y/n]')
-	print no_metadata
-	if no_metadata != 'Y' and no_metadata != 'yes' and no_metadata != 'Yes':
-		logging.error('User chose to exit because no metadata file provided for the specified files')
-		sys.exit(0)
-	else:
-		logging.error('User chose to upload files without metadata')
 elif args.mode == 'update' and args.metadata is None:
 	logging.error('Mode chosen is UPDATE and no metadata file is provided')
 	sys.exit(0)
 
-
-# def updateItems(metadata_file):
-# 	metadata = load_metadata_file(metadata_file)
-
+walk_files_upload(args.files)
 
 # # Headers : add additional HTTP headers to the request if needed
 # # RTFM : http://archive.org/help/abouts3.txt
 # headers = dict()
-
-
 
 # # Get the item with unique identifier. The item will be created if it does not exist.
 # item = internetarchive.get_item(item_id)
