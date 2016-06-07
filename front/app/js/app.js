b5514f79d4ec05a435ed3a624bdbd7bd89d2423d
Anne Lhote <anne.lhote@gmail.com>
2016-06-07 12:22:29 +0200

    Add rest communication between AngularJS interface and Python script with a Flask server

--- front/app/js/app.js
+++ front/app/js/app.js
@@ -5,9 +5,22 @@
     var app = angular.module('internetarchive', ['ngRoute']);
 
     /* Default Page Controller */
-    app.controller('IAController', ['$scope',
-        function($scope) {
+    app.controller('IAController', ['$scope', '$http',
+        function($scope, $http) {
             $scope.actions = ['create', 'update', 'delete', 'visualize'];
+
+            $scope.submit = function() {
+                if ($scope.action === undefined) {
+                    alert('Please choose an action to process !');
+                } else {
+                    $http.get('http://localhost:5000/create').success(function(data, status, headers, config) {
+                        console.log(data);
+                    }).error(function(data, status, headers, config) {
+                        console.log('Error');
+                        console.log(arguments);
+                    });
+                }
+            }
         }
     ]);
 
---
+++ python/server.py
@@ -0,0 +1,72 @@
+# !/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import datetime
+import flask
+import functools
+import iamassaccess
+
+app = flask.Flask(__name__)
+
+# Croosdomain decorator to enable cross domain request
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, datetime.timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = flask.current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and flask.request.method == 'OPTIONS':
+                resp = flask.current_app.make_default_options_response()
+            else:
+                resp = flask.make_response(f(*args, **kwargs))
+            if not attach_to_all and flask.request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return functools.update_wrapper(wrapped_function, f)
+    return decorator
+
+@app.route("/create")
+@crossdomain(origin='*')
+def create():
+    # Headers : add additional HTTP headers to the request if needed
+    # RTFM : http://archive.org/help/abouts3.txt
+    headers = dict()
+    return iamassaccess.createItems(args.folder, headers)
+
+@app.route("/update")
+@crossdomain(origin='*')
+def create():
+    return iamassaccess.updateItems(args.metadata)
+
+@app.route("/delete")
+@crossdomain(origin='*')
+def create():
+    return iamassaccess.deleteItems(args.metadata)
+
+if __name__ == "__main__":
+    app.run()
