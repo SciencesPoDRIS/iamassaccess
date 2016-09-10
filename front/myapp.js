52c13e34f440ef4c428ed2f6e5e56a9eb7e92352
Anne Lhote <anne.lhote@gmail.com>
2016-09-10 16:52:31 +0200

    Enable upload zip folder

--- front/myapp.js
+++ front/myapp.js
@@ -1,27 +1,46 @@
-angular.module("myapp", ['ngMaterial', 'ngCsvImport', 'myapp.directives', 'myapp.client', 'ngTable'])
- 
-.controller("MainControl", ['$scope', 'api', 'NgTableParams', function($scope, api, NgTableParams) {
-    $scope.lol = "idontevenunderstandanything";
-    $scope.csv = {
-      content: null,
-      header: null,
-      headerVisible: true,
-      separator: ',',
-      separatorVisible: true,
-      result: null,
-      encoding: 'utf8',
-      encodingVisible: true
-    };
-    $scope.zipfile = null;
-    $scope.ngparam = new NgTableParams({}, { dataset: $scope.content });
+(function() {
+    'use strict';
 
-    // Test
-    api.upload();
- }]);
+    var app = angular.module("myapp", ['ngMaterial', 'ngCsvImport', 'myapp.directives', 'ngTable', 'ngFileUpload']);
 
-// .config(function($mdThemingProvider) {
-//   $mdThemingProvider.theme('default')
-//     .primaryPalette('pink')
-//     .accentPalette('orange');
-// });
+    app.controller("MainControl", ['$scope', 'NgTableParams', 'Upload', function($scope, NgTableParams, Upload) {
+        $scope.csv = {
+            content: null,
+            header: null,
+            headerVisible: true,
+            separator: ',',
+            separatorVisible: true,
+            result: null,
+            encoding: 'utf8',
+            encodingVisible: true
+        };
+        $scope.zipfile = null;
+        $scope.ngparam = new NgTableParams({}, { dataset: $scope.content });
 
+        // upload later on form submit or something similar
+        $scope.submit = function() {
+            if ($scope.form.file.$valid && $scope.file) {
+                $scope.upload($scope.file);
+            }
+        };
+
+        // upload on file select or drop
+        $scope.upload = function(file) {
+            Upload.upload({
+                url: 'http://localhost:5000/upload',
+                data: {file: file},
+                method: 'POST'
+            }).then(function(resp) {
+                console.log('Success');
+                console.log(resp);
+                // console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+            }, function(resp) {
+                console.log('Error status: ' + resp.status);
+            }, function(evt) {
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+            });
+        };
+    }]);
+
+})();
